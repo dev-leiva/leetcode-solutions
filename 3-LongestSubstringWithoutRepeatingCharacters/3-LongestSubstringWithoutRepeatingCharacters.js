2a757bde2b1233c9aa6b863bad8f4c85a7268ed2
dev-leiva <juan.ferraressoleiva+github@gmail.com>
2025-10-06 18:57:09 -0300

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,21 @@
+// Last updated: 10/6/2025, 6:57:15 PM
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    var k = 0;
+    var maxLength = 0;
+    for(i = 0; i < s.length; i++) {
+        for (j = k; j < i; j++) {
+            if (s[i] === s[j]) {
+                k = j + 1;
+                break;
+            }
+        }
+        if (i - k + 1 > maxLength) {
+            maxLength = i - k + 1;
+        }
+    }
+    return maxLength;
+};
